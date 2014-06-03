ecfc6a061c62c8b488d4cf892042d02a7aaa14ea
luoyfan <347833225@qq.com>
2014-06-03 19:11:47 +0800

    database

---
+++ src/LetsGo/src/com/project/letsgo/DBAdapter.java
@@ -0,0 +1,115 @@
+package com.project.letsgo;
+
+import java.util.ArrayList;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBAdapter extends SQLiteOpenHelper{
+	public static final String DB_NAME = "record.db";
+	public static final int DB_VERSION = 1;
+	public static final String TABLE_NAME = "plan";
+	public static final String KEY_ID = "_id";
+	public static final String KEY_STATE = "state";
+	public static final String KEY_DATE = "date";
+	public static final String KEY_START_TIME = "startTime";
+	public static final String KEY_END_TIME = "endTime";
+	
+
+	public DBAdapter(Context context) {
+		super(context, DB_NAME, null, DB_VERSION);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		String SQL_CREATE_TABLE = "create table " + TABLE_NAME
+									+ " ("+KEY_ID+" integer primary key autoincrement,"
+									+ " "+KEY_STATE+" integer,"
+									+ " "+KEY_DATE+" TEXT,"
+									+ " "+KEY_START_TIME+" TEXT,"
+									+ " "+KEY_END_TIME+" TEXT);";
+		db.execSQL(SQL_CREATE_TABLE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private ContentValues recordToValues(ContentValues values, Record record)
+	{
+		values.put(KEY_STATE, record.getState());
+		values.put(KEY_DATE, record.getDate());
+		values.put(KEY_START_TIME, record.getStartTime());
+		values.put(KEY_END_TIME, record.getEndTime());
+		return values;
+	}
+	
+	private Record valuesToRecord(Record record, Cursor cursor)
+	{
+		record.setID(cursor.getInt(cursor.getColumnIndex(KEY_ID)));
+		record.setState(cursor.getInt(cursor.getColumnIndex(KEY_STATE)));
+		record.setDate(cursor.getString(cursor.getColumnIndex(KEY_DATE)));
+		record.setStartTime(cursor.getString(cursor.getColumnIndex(KEY_START_TIME)));
+		record.setEndTime(cursor.getString(cursor.getColumnIndex(KEY_END_TIME)));
+		return record;
+	}
+	
+	public long insert(Record record)
+	{
+		SQLiteDatabase db = getWritableDatabase();
+		return db.insert(
+							TABLE_NAME, 
+							null, 
+							recordToValues(new ContentValues(), record)
+						);
+	}
+	
+	public long deleteOneData(int id)
+	{
+		SQLiteDatabase db = getWritableDatabase();
+		return db.delete(
+							TABLE_NAME, 
+							KEY_ID+"="+id, 
+							null
+						);
+	}
+	
+	public long updateOneData(int id, Record record)
+	{
+		SQLiteDatabase db = getWritableDatabase();
+		return db.update(
+							TABLE_NAME, 
+							recordToValues(new ContentValues(), record), 
+							KEY_ID+"="+id, 
+							null
+						);
+	}
+	
+	public ArrayList<Record> convertToRecord(Cursor cursor)
+	{
+		int resultCounts = cursor.getCount();
+		if (resultCounts == 0 || !cursor.moveToFirst())
+		{
+			return null;
+		}
+		ArrayList<Record> records = new ArrayList<Record>();
+		for (int i = 0; i < resultCounts; i++)
+		{
+			records.add( valuesToRecord(new Record(), cursor) );
+			cursor.moveToNext();
+		}
+		return records;
+	}
+	
+	public Cursor getCursor()
+	{
+		return getReadableDatabase().query(TABLE_NAME, null, null, null, null, null, null);
+	}
+}
--- src/LetsGo/src/com/project/letsgo/Record.java
+++ src/LetsGo/src/com/project/letsgo/Record.java
@@ -1,18 +1,36 @@
 package com.project.letsgo;
 
 public class Record {
+	public static final int NOT_COMPLETE = 0;
+	public static final int SUCCESS = 1;
+	public static final int FAIL = 2;
 	
+	private int id;
 	private int state; // 0 for not complete; 1 for success; 2 for fail
 	private String date;
 	private String start_time;
 	private String end_time;
 	
+	public Record()
+	{
+		id = -1;
+		state = Record.NOT_COMPLETE;
+		date = "";
+		start_time = "";
+		end_time = "";
+	}
+	
 	public Record(String d, String s, String e)
 	{
-		state = 0;
+		id = -1;
+		state = Record.NOT_COMPLETE;
 		date = d;
 		start_time = s;
 		end_time = e;
+	}
+	
+	public int getID(){
+		return id;
 	}
 	
 	public int getState(){
@@ -31,10 +49,25 @@ public class Record {
 		return end_time;
 	}
 	
+	public void setID(int i){
+		id = i;
+	}
+	
 	public void setState(int i){
 		state = i;
 	}
 	
+	public void setDate(String s){
+		date = s;
+	}
+	
+	public void setStartTime(String s){
+		start_time = s;
+	}
+	
+	public void setEndTime(String s){
+		end_time = s;
+	}
 	// debug use
 	public void printInfo(){
 		System.out.print("" + state + " " + date + " " + start_time + " " + end_time);
